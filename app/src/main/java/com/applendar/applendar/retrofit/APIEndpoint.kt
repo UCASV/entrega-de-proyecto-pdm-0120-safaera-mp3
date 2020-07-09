5fa6694d522784870a12aa116c80e244eb135aa6
EdwinM96 <edwin.morales9b@gmail.com>
2020-07-09 16:46:12 -0600

    Added RetroFit method to listado materias. Pending implementation

--- app/src/main/java/com/applendar/applendar/retrofit/APIEndpoint.kt
+++ app/src/main/java/com/applendar/applendar/retrofit/APIEndpoint.kt
@@ -1,6 +1,7 @@
 package com.applendar.applendar.retrofit
 
 import com.applendar.applendar.domain.Actividad
+import com.applendar.applendar.domain.Materia
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -11,4 +12,6 @@ interface APIEndpoint {
     @GET("/actividad/")
     fun getActividades(@Query("materiaId") materiaId: Integer):Call<ArrayList<Actividad>>
 
+    @GET("/materia/")
+    fun getMaterias():Call<ArrayList<Materia>>
 }
