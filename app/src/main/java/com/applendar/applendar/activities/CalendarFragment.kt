ac55c37c3afde62e08c0fd59793830074c71795b
00400815@uca.edu.sv <16137238>
2020-07-15 21:02:08 -0600

    presice..

--- app/src/main/java/com/applendar/applendar/activities/CalendarFragment.kt
+++ app/src/main/java/com/applendar/applendar/activities/CalendarFragment.kt
@@ -1,10 +1,13 @@
 package com.applendar.applendar.activities
 
+import android.os.AsyncTask
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
@@ -12,10 +15,9 @@ import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
 
 import com.applendar.applendar.R
-import com.applendar.applendar.adapters.Activity
+//import com.applendar.applendar.domain.Materia
 import com.applendar.applendar.adapters.AgendaAdapter
 import com.applendar.applendar.databinding.FragmentCalendarBinding
-import com.applendar.applendar.databinding.FragmentPresentationBinding
 import com.kizitonwose.calendarview.model.CalendarDay
 import com.kizitonwose.calendarview.model.DayOwner
 import com.kizitonwose.calendarview.ui.DayBinder
@@ -25,20 +27,38 @@ import org.threeten.bp.YearMonth
 import org.threeten.bp.format.DateTimeFormatter
 import androidx.core.view.children
 import androidx.core.view.isVisible
-import com.applendar.applendar.activities.daysOfWeekFromLocale
-import com.applendar.applendar.activities.makeInVisible
-import com.applendar.applendar.activities.makeVisible
-import com.applendar.applendar.activities.setTextColorRes
+import com.applendar.applendar.MainActivity
+import com.applendar.applendar.adapters.MateriasAdapter
 import com.kizitonwose.calendarview.model.CalendarMonth
 import com.kizitonwose.calendarview.ui.MonthHeaderFooterBinder
 import com.kizitonwose.calendarview.ui.ViewContainer
 import com.applendar.applendar.databinding.Example3CalendarDayBinding
 import com.applendar.applendar.databinding.Example3CalendarHeaderBinding
 import com.applendar.applendar.databinding.Example3FragmentBinding
+import com.applendar.applendar.domain.Actividad
+import com.applendar.applendar.domain.Materia
+import com.applendar.applendar.retrofit.APIEndpoint
+import com.applendar.applendar.retrofit.AsyncResponse
+import com.applendar.applendar.util.GeneralGlobalVariables
+import kotlinx.android.synthetic.main.fragment_calendar.progressBar
+import kotlinx.android.synthetic.main.fragment_web_view.*
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Call
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.io.IOException
 
 data class Event(val id: String, val text: String, val date: LocalDate)
 
 class CalendarFragment : Fragment() {
+    lateinit var materias: ArrayList<Actividad>;
+    lateinit var materiasPerma: ArrayList<Actividad>
+    lateinit var filteredmateriasPerma: ArrayList<Actividad>;
+    lateinit var loadingModal: LinearLayout
+    lateinit var materiaId: Integer
+
 
     private var selectedDate: LocalDate? = null
     private val today = LocalDate.now()
@@ -68,29 +88,52 @@ class CalendarFragment : Fragment() {
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
         super.onViewCreated(view, savedInstanceState)
+//===========apiREST <> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <>
+        materiasPerma = ArrayList<Actividad>()
+        materiaId = 1 as Integer
+        var obj = object : AsyncResponse {
+            override fun processFinish() {
+                Toast.makeText(context, "Loading", Toast.LENGTH_SHORT).show()
+                //materiasPerma.addAll(materias)
+                //Log.d("restar","fecha -> ${materias.get(0).fechaDeEvaluacion}")
+
+            }
+
+            override fun alternativeFinish() {
+                Toast.makeText(context, "Sorry, an error occurred", Toast.LENGTH_SHORT).show()
+
+            }
+
+        }
+        ActividadesRequest(obj).execute()
+//===========apiREST<> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <>
 
 
         //============================activitites Recyclerview events of month
+
+        var activitesList = materiasPerma.filter {
+            it.fechaDeEvaluacion == binding.exThreeSelectedDateText.text
+        }
+        filteredmateriasPerma = ArrayList(activitesList)
+
+
+        val adapter = AgendaAdapter(filteredmateriasPerma)
+        act_recycler.adapter = adapter
+
         //val recycler: RecyclerView = findViewById(R.id.recycler)
         //recycler.layoutManager = LinearLayoutManager(this,RecyclerView.VERTICAL, false)
         act_recycler.addItemDecoration(DividerItemDecoration(context, RecyclerView.VERTICAL))
+        //var mat: Materia()
 
-        val activities = ArrayList<Activity>()
-        activities.add(Activity("Examen", "moviles", "23/06/2020", R.drawable.quiz))
-        activities.add(Activity("Examen", "moviles", "23/06/2020", R.drawable.quiz))
-        activities.add(Activity("Examen", "moviles", "23/06/2020", R.drawable.quiz))
-        activities.add(Activity("Examen", "moviles", "23/06/2020", R.drawable.quiz))
-        activities.add(Activity("Examen", "moviles", "23/06/2020", R.drawable.quiz))
-        activities.add(Activity("Examen", "moviles", "23/06/2020", R.drawable.quiz))
-        activities.add(Activity("Examen", "moviles", "23/06/2020", R.drawable.quiz))
-        activities.add(Activity("Examen", "moviles", "23/06/2020", R.drawable.quiz))
-        activities.add(Activity("Examen", "moviles", "23/06/2020", R.drawable.quiz))
-        activities.add(Activity("Examen", "moviles", "23/06/2020", R.drawable.quiz))
+        val activities = ArrayList<Actividad>()
+        //activities.add(Actividad(0,"corto 1","evaluacion", "moviles", "23/06/2020", mat ))
 
 
-        val adapter = AgendaAdapter(activities)
-        act_recycler.adapter = adapter
+//        val adapter = AgendaAdapter(activities)
+//        val adapter = AgendaAdapter(materiasPerma)
+//        act_recycler.adapter = adapter
 
         //============================activitites Rv
 
@@ -160,11 +203,16 @@ class CalendarFragment : Fragment() {
         }
 
         binding.exThreeCalendar.monthScrollListener = {
-//            homeActivityToolbar.title = if (it.year == today.year) {      //stuff of text on toolbar
-//                titleSameYearFormatter.format(it.yearMonth)
-//            } else {
-//                titleFormatter.format(it.yearMonth)
-//            }
+            var mainActivity: MainActivity = (activity as MainActivity);
+            // (activity as MainActivity).setActionBarTitle("Actividades del mes")
+
+            var monthz = if (it.year == today.year) {      //stuff of text on toolbar
+                titleSameYearFormatter.format(it.yearMonth)
+            } else {
+                titleFormatter.format(it.yearMonth)
+            }
+
+            (activity as MainActivity).setActionBarTitle(monthz.toUpperCase())
 
             // Select the first day of the month when
             // we scroll to a new month.
@@ -239,4 +287,53 @@ class CalendarFragment : Fragment() {
 //        homeActivityToolbar.setBackgroundColor(requireContext().getColorCompat(R.color.colorPrimary))
 //        requireActivity().window.statusBarColor = requireContext().getColorCompat(R.color.colorPrimaryDark)
 //    }
+    //=================================
+
+    inner class ActividadesRequest(callback: AsyncResponse) :
+        AsyncTask<String?, Void?, ArrayList<Actividad>>() {
+        var delegate: AsyncResponse
+
+        override fun onPostExecute(result: ArrayList<Actividad>?) {
+            delegate.processFinish()
+        }
+
+        override fun doInBackground(vararg p0: String?): ArrayList<Actividad>? {
+            val interceptor = HttpLoggingInterceptor()
+            interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+            val client = OkHttpClient.Builder()
+                .addInterceptor(interceptor).build()
+            val BASE_URL: String = GeneralGlobalVariables.BASE_URL
+            val retrofit = Retrofit.Builder()
+                .client(client)
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+            val apiService: APIEndpoint = retrofit.create(APIEndpoint::class.java)
+
+            /*val encoder = Encoder()
+            val usuario = prefs.getString(LoginActivity.USERNAME, null)
+            val password = prefs.getString(LoginActivity.PASSWORD, null)
+            val encodedString: String = encoder.encoder64(usuario, password)*/
+            val request: Call<ArrayList<Actividad>> =
+                apiService.getActividades(materiaId)
+            val response: Response<ArrayList<Actividad>>
+            try {
+                response = request.execute()
+                materias = response.body()!!;
+                materiasPerma.addAll(materias)
+                return response.body()
+            } catch (e: IOException) {
+                println(e.message)
+            }
+            return null
+        }
+
+
+        init {
+            delegate = callback
+        }
+
+    }
+
+
 }
